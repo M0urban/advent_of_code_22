25732430e83e7158e13ec780300530d806b7b4ba
Marius Urban <murban@stud.hs-offenburg.de>
2022-12-01 10:55:16 +0100

    finished challenge 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -2,12 +2,13 @@ use std::io::{self, BufRead, Read};
 use std::fs::File;
 
 fn main() {
-    let calories = most_calories("day1.txt");
+    let calories = most_calories("./day1.txt");
+    println!("{}", calories);
+    let calories = top3_calories("./day1.txt");
     println!("{}", calories);
 }
 
 fn most_calories(file: &str) -> u32 {
-    
     let reader = io::BufReader::new(File::open(file).unwrap());
     let mut max_calories = 0u32;
     let mut current_calories = 0u32;
@@ -28,4 +29,35 @@ fn most_calories(file: &str) -> u32 {
         max_calories = current_calories;
     }
     max_calories
+}
+
+fn top3_calories(file: &str) -> u32 {
+    let reader = io::BufReader::new(File::open(file).unwrap());
+    let mut max_calories = [0u32; 3];
+    let mut current_calories = 0u32;
+
+    for slice in  reader.lines() {
+        match slice.unwrap().parse::<u32>() {
+            Ok(num) => current_calories += num,
+            Err(_) => {
+                max_calories.sort();
+                'l: for cal in max_calories.iter_mut() {
+                    if *cal < current_calories{
+                        *cal = current_calories;
+                        break 'l;
+                    }
+                }
+                current_calories = 0;
+            },
+        }
+
+    }
+    max_calories.sort();
+    'l: for cal in max_calories.iter_mut() {
+        if *cal < current_calories{
+            *cal = current_calories;
+            break 'l;
+        }
+    }
+    max_calories[0] + max_calories[1] + max_calories[2]
 }
